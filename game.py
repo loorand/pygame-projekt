8f228b1b2edabf656f8a14bdb8ad3818418fd36a
loorand <145984471+loorand@users.noreply.github.com>
2023-11-11 17:55:55 +0200

    Update game.py

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from sys import exit
 
+# function to change and display score during gameplay
 def scores():
     global score
     # collision
@@ -13,32 +14,39 @@ def scores():
     score_rect = score_surface.get_rect(center = (500,69))
     screen.blit(score_surface, score_rect)
 
+# pygame initialisation and display config (size, used fonts)
 pygame.init()
 screen = pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("Fly Game")
 clock = pygame.time.Clock()
 font = pygame.font.Font("fonts/RobotoMono-Semibold.ttf", 50)
 
+# background surface
 bg_surface = pygame.image.load("graphics/background.png").convert_alpha()
 
 score = 1000
 
+# square surface / spawn location
 square_surface = pygame.image.load("graphics/square.png").convert_alpha()
 square_rect = square_surface.get_rect(midleft = (random.randint(0, 1000), random.randint(25, 975)))
 
+# healer surface / spawn location
 heal_surface = pygame.image.load("graphics/heal.png").convert_alpha()
 heal_rect = heal_surface.get_rect(midleft = (random.randint(1000, 5000), random.randint(25, 975)))
 
+# player surface / spawn location
 player_surface = pygame.image.load("graphics/player.png").convert_alpha()
 player_rect = player_surface.get_rect(center = (500,500))
 
 game = True
+# main loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
+        # game restart screen (S to restart)
         if not game:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
                 game = True
@@ -47,8 +55,8 @@ while True:
                 heal_rect = heal_surface.get_rect(midleft = (random.randint(1000, 5000), random.randint(25, 975)))
                 player_rect = player_surface.get_rect(center = (500,500))
 
-    if game:   
-        # movement
+    if game:
+        # player movement
         player_speed = 12
         keys = pygame.key.get_pressed()
         if keys[pygame.K_f]:
@@ -62,7 +70,7 @@ while True:
         if keys[pygame.K_RIGHT]:
             player_rect.x += player_speed
 
-        # background & text
+        # background & text/score function
         screen.blit(bg_surface,(0,0))
         scores()
         
@@ -78,6 +86,7 @@ while True:
             heal_rect.left = 1000
             heal_rect.y = random.randint(25, 975)
 
+        # collision
         #if player_rect.colliderect(square_rect):
         #    player_rect.right = square_rect.left
         
@@ -91,15 +100,15 @@ while True:
         if player_rect.bottom <= 20:
             player_rect.bottom = 20
 
-        # square & player
+        # square & player blit
         screen.blit(square_surface,square_rect)
         screen.blit(heal_surface,heal_rect)
         screen.blit(player_surface,player_rect)
 
-        # game state
+        # game over
         if score <= 0:
             game = False
-    
+
     else:
         screen.fill("#111111")
 
